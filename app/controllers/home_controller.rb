691008cd578086bdb19e9f074e5119470839786a
alloLaPlanete <simonolivierharel@gmail.com>
2017-06-08 21:23:04 +0000

    Fin de la configuration administrateur

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
   
-
-load_and_authorize_resource
+#load_and_authorize_resource
   
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000}
+  config.action_mailer.default_url_options = { host: 'localhost', port: 8080}
   
   #config.action_mailer.default_url_options = { host: '137.74.175.51', port: 3001 }
   
@@ -48,13 +48,14 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   
   #config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
   
   config.action_mailer.delivery_method = :smtp
   
   config.action_mailer.smtp_settings = {
     user_name:      Rails.application.secrets.mail_username,
     password:       Rails.application.secrets.mail_password,
-    domain:         'gmail.com',
+    domain:        'gmail.com',
     address:       'smtp.gmail.com',
     port:          '587',
     authentication: :plain,
