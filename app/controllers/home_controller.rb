b179654cf2a8d6fe199e093d5600f61f71c57722
simonHarel <simonolivierharel@gmail.com>
2017-06-27 23:21:51 +0000

    formulaire de creation dusager

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-    
+
     def index
        @users = User.all
     end
@@ -8,22 +8,40 @@ class HomeController < ApplicationController
          @user = User.new
     end
   
-   def create
-    @user = User.new(user_params)
-    if @user.save
-      flash[:notice] = "Employer a ete cree"
-      flash[:color]= "valid"
-    else
-      flash[:notice] = "Form is invalid"
-      flash[:color]= "invalid"
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            flash[:notice] = "Employe a ete cree"
+            flash[:color]= "valid"
+        
+        
+        else
+            flash[:notice] = "Form is invalid"
+            flash[:color]= "invalid"
+        end
+        
+        # render "new"
+        redirect_to('/home/index')
     end
-   # render "new"
-   redirect_to('/home/index')
-  end
+
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+    def edit
+        @user = User.find(params[:id])
+    end
+    
+    private 
+    def user_params
+        params.require(:user).permit(:email, :password, :role)
+    end
   
-  private 
-  def user_params
-      params.require(:user).permit(:email, :password, :role)
-  end
   
+    def destroy
+     @user.destroy
+    end
+
 end
+
--- db/migrate/20170607014321_add_roles_to_users.rb
+++ db/migrate/20170607014321_add_roles_to_users.rb
@@ -1,7 +1,8 @@
 class AddRolesToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :superadmin_role, :boolean , default: false
-    add_column :users, :supervisor_role, :boolean , default: false
-    add_column :users, :user_role, :boolean , default: true
+    add_column :users, :directeur_role, :boolean , default: false
+    add_column :users, :coordonateur_role, :boolean , default: false
+    add_column :users, :adjoint_coordonateur_role, :boolean , default: true
+    add_column :users, :intervenant_role, :boolean , default: false
   end
 end
